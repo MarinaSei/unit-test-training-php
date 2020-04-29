bbaacd052e64a827fca73d41673642198c8edb22
Marina <marina.seiika@gmail.com>
2020-04-29 12:07:48 +0200

    Documentation php exercice

--- src/Email.php
+++ src/Email.php
@@ -1,9 +1,25 @@
 <?php
 
+/**
+ * @file Email.php
+ * @author Mohamed
+ * @version 1.1
+ */
+
+ /**
+  * @class Email Email.php
+  */
 final class Email
 {
+    /**
+     * @attribut string $email
+     */
     private $email;
 
+    /**
+     * Email constructor.
+     * @param $email string
+     */
     private function __construct(string $email)
     {
         $this->ensureIsValidEmail($email);
@@ -11,16 +27,24 @@ final class Email
         $this->email = $email;
     }
 
+    /**
+     * @param $email string
+     * @return string
+     */
     public static function fromString(string $email): self
     {
         return new self($email);
     }
 
+    /**
+     * @return string
+     */
     public function __toString(): string
     {
         return $this->email;
     }
 
+    
     private function ensureIsValidEmail(string $email): void
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
--- src/Salary.php
+++ src/Salary.php
@@ -1,8 +1,19 @@
 <?php
+/**
+ * @file Email.php
+ * @author Mohamed
+ * @version 1.1
+ */
 
-
+ /**
+ * @class Salary Salary.php
+ */
 class Salary
 {
+    /**
+     * Fonction calculNetSalary qui calcule le salaire net
+     * @return int le salaire net
+     */
     static public function calculNetSalary($grossMonthlySalary)
     {
         return ($grossMonthlySalary * 55) / 100;
